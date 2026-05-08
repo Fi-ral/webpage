f626fc0be6992625d382ef4dcec6976ecc931661
Fiiral <tylerwolff05@gmail.com>
2026-05-08 21:05:13 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,117 @@
+import urllib.parse
+import subprocess
+import mimetypes
+import threading
+import socket
+import os
+
+HOST = '127.0.0.1'
+PORT = 8079
+RUNNING = True
+
+#############################################
+### THIS IS A VERY VERY UGLY TEST SERVER! ###
+#############################################
+#          Do not use it please :3          #
+#############################################
+
+def serve_file(fp: str, error: bool=False) -> bytes | None:
+    content_type, _ = mimetypes.guess_type(fp)
+
+    if os.path.isfile(fp):
+        with open(fp, "rb") as f:
+            body = f.read()
+
+        if error:
+            return f"HTTP/1.1 404 Not Found\r\nContent-Type: {content_type}\r\n\r\n".encode() + body
+        return f"HTTP/1.1 200 OK\r\nContent-Type: {content_type}\r\n\r\n".encode() + body
+    else:
+        return None
+
+def handle_get_requests(path: str) -> bytes:
+    file_path = os.path.join('./public/', path)
+    if not os.path.splitext(path)[1]:
+        file_path += ".html"
+
+    response = serve_file(file_path)
+
+    if path == "":
+        return serve_file("public/index.html") # type: ignore
+    if response == None:
+        return serve_file("public/404.html", True) # type: ignore
+    return response
+
+def handle_head_requests(path: str) -> bytes:
+    file_path = os.path.join('./public/', path)
+
+    if not os.path.splitext(path)[1]:
+        file_path += ".html"
+    if path == "":
+        file_path = "index.html"
+    if not os.path.exists(file_path):
+        file_path = "404.html"
+        if not os.path.exists(file_path):
+            return b"HTTP/1.1 404 Not Found\r\nContent-Length: 0\r\n\r\n"
+
+    size = os.path.getsize(file_path)
+    return f"HTTP/1.1 200 OK\r\nContent-Length: {size}\r\n\r\n".encode()
+
+def handle_request(request: str) -> bytes:
+    try:
+        lines = request.split("\r\n")
+        method, path, _ = lines[0].split()
+        path = urllib.parse.unquote(path).lstrip('/').split('?')[0]
+
+        try:
+            print(f"Serving: {' '.join(lines[0:3])}")
+        except:
+            pass
+
+        if method == "GET":
+            return handle_get_requests(path)
+        if method == "HEAD":
+            return handle_head_requests(path)
+        else:
+            return f"HTTP/1.1 405 Method Not Allowed\r\nContent-Type: text/plain".encode()
+    except Exception as e:
+        print(e)
+        return f"HTTP/1.1 500 Internal Server Error\r\nContent-Type: text/plain".encode()
+
+def compile_typescript():
+    try:
+        print("Compiling TypeScript...")
+        subprocess.run(['npx', 'tsc'], check=True)
+        print("TypeScript compilation completed.\n")
+    except subprocess.CalledProcessError as e:
+        print(f"TypeScript compilation failed because {e}")
+        os._exit(1)
+
+def client_thread(client_socket: socket.socket):
+    with client_socket:
+        data = b""
+        while b"\r\n\r\n" not in data:
+            chunk = client_socket.recv(1024)
+            if not chunk:
+                break
+            data += chunk
+        response = handle_request(data.decode())
+        client_socket.sendall(response)
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM, 0) as server_socket:
+    compile_typescript()
+
+    server_socket.bind((HOST, PORT))
+    server_socket.listen(1)
+
+    print(f"\nServing on http://{HOST}:{PORT}\n")
+
+    try:
+        while True:
+            client_socket, _ = server_socket.accept()
+            thread = threading.Thread(target=client_thread, args=(client_socket,))
+            thread.daemon = True
+            thread.start()
+    except KeyboardInterrupt:
+        print("\nServer shutting down.")
+    finally:
+        server_socket.close()
---
+++ src/base.ts
@@ -0,0 +1,107 @@
+function toggleReveal(id: number) {
+	const list = document.getElementById(`hidden-${id}`);
+	const btn = document.getElementById(`button-${id}`);
+
+	if (!list || !btn) {
+        console.error("Could not find btn or list!")
+        return;
+    }
+
+	if (list.style.display === "none") {
+		list.style.display = "block";
+		btn.innerText = "- Hide";
+	} else {
+	    list.style.display = "none";
+	    btn.innerText = "+ Show";
+	}
+}
+
+function createSideboard() {
+	const sideboard = document.getElementById("sideboard-container");
+	if (!sideboard) {
+        console.error("Could not find sideboard!")
+        return;
+    }
+
+	sideboard.innerHTML = `<section id="sideboard">
+			<div>
+			<h3>Quicklinks</h3>
+			<ul>
+				<li><span><a href="/">Home</a></span></li>
+			</ul>
+			</div>
+			<br>
+	
+			<div>
+			<h3>Projects</h3>
+			<button class="show-more-button" id="button-1" onclick="toggleReveal(1)">+ Show</button>
+			<ul id="hidden-1" style="display: none;">
+				<li><span><a href="/projects/final_frontier.html">The Final Frontier</a></span></li>
+				<li><span><a href="/projects/dust_and_dawn.html">Dust & Dawn</a></span></li>
+			</ul>
+			</div>
+			<br>
+
+			<div>
+				<h3>Articles</h3>
+				<button class="show-more-button" id="button-2" onclick="toggleReveal(2)">+ Show</button>
+				<ul id="hidden-2" style="display: none;">
+					<li><span><a href="/articles/planet_classifications.html">Planet Classifications</a></span></li>
+				</ul>
+			</div>
+			<br>
+
+			<div>
+				<h3>Tools</h3>
+				<h4>General</h4>
+				<button class="show-more-button" id="button-3" onclick="toggleReveal(3)">+ Show</button>
+				<ul id="hidden-3" style="display: none;">
+					<li><span><a href="/tools/general/case_converter.html">Case Converter</a></span></li>
+				</ul>
+
+				<h4>Space</h4>
+				<button class="show-more-button" id="button-4" onclick="toggleReveal(4)">+ Show</button>
+				<ul id="hidden-4" style="display: none;">
+					<li><span><a href="/tools/space/habitats.html">Habitat Calculator</a></span></li>
+					<li><span><a href="/tools/space/brachistochrone.html">Brachistochrone Calculator</a></span></li>
+					<li><span><a href="/tools/space/travel_time.html">Travel Time Calculator</a></span></li>
+					<li><span><a href="/tools/space/engines.html">Engine Calculator</a></span></li>
+					<li><span><a href="/tools/space/radiators.html">Radiator Calculator</a></span></li>
+				</ul>
+
+				<h4>Japanese</h4>
+				<button class="show-more-button" id="button-5" onclick="toggleReveal(5)">+ Show</button>
+				<ul id="hidden-5" style="display: none;">
+					<li><span><a href="/tools/japanese/romaji.html">Wapuro Romaji Converter</a></span></li>
+					<li><span><a href="/tools/japanese/conjugations.html">Conjugation Trainer</a></span></li>
+					<li><span><a href="/tools/japanese/counters.html">Counter Trainer</a></span></li>
+				</ul>
+			</div>
+		</section>`;
+}
+
+function main() {
+    createSideboard();
+
+    function openSidebar() {
+        sidebar.classList.add('open');
+        toggle.textContent = '✕';
+        toggle.setAttribute('aria-expanded', 'true');
+    }
+    function closeSidebar() {
+        sidebar.classList.remove('open');
+        toggle.textContent = '☰';
+        toggle.setAttribute('aria-expanded', 'false');
+    }
+
+    const sidebar = document.getElementById('sideboard')!
+    const toggle = document.getElementById('sidebarToggle')!;
+
+    toggle.addEventListener('click', () => {
+        sidebar.classList.contains('open') ? closeSidebar() : openSidebar();
+    });
+
+    document.addEventListener('keydown', e => {
+        if (e.key === 'Escape') closeSidebar();
+    });
+}
